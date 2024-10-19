82d7859567a8fb90ac8338d04b65d6ef1ab6d883
Pau Corzo <paucorzo19@gmail.com>
2024-10-19 03:52:15 -0600

    follow mouse
    ya sigue el mouseee :D

--- Fundamentos Programacion/Assets/Proyectos/PeggleIsh/Scripts/Shooter.cs
+++ Fundamentos Programacion/Assets/Proyectos/PeggleIsh/Scripts/Shooter.cs
@@ -7,21 +7,30 @@ public class Shooter : MonoBehaviour
     public GameObject Pewpew;
     public float Force = 10.0f;
     public Rigidbody2D rb;
- 
+    public Transform firePoint;
+
+    private Vector2 mouseDir;
+
+    
 
     public void Update()
     {
+        mouseDir = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.localRotation = Quaternion.LookRotation(Vector3.forward, mouseDir - (Vector2)transform.position);
         Shoot();
     }
-
+    public void FixedUpdate()
+    {
+        rb.velocity = transform.up * Force;
+    }
     public void Shoot()
     {
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
-            GameObject ballInstance = Instantiate(Ball, transform.position, Quaternion.identity);
+            GameObject ballInstance = Instantiate(Ball, firePoint.position, firePoint.rotation );
             Rigidbody2D rb = ballInstance.GetComponent<Rigidbody2D>();
             
-            rb.AddForce(Vector2.down * Force, ForceMode2D.Impulse);
+            rb.AddForce(Vector2.down * Force, ForceMode2D.Impulse); //aqui es el pobema peeeero no se como arreglarlo xd
            
         }
     }
