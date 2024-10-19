a1fb1e9bae01118558b7ab814aeec56c4e08fd81
Pau Corzo <paucorzo19@gmail.com>
2024-10-19 02:58:13 -0600

    shooter ready
    ya hace pew pew pew :3

--- Fundamentos Programacion/Assets/Proyectos/PeggleIsh/Scripts/Shooter.cs
+++ Fundamentos Programacion/Assets/Proyectos/PeggleIsh/Scripts/Shooter.cs
@@ -6,14 +6,23 @@ public class Shooter : MonoBehaviour
     public GameObject Ball;
     public GameObject Pewpew;
     public float Force = 10.0f;
+    public Rigidbody2D rb;
+ 
+
+    public void Update()
+    {
+        Shoot();
+    }
 
     public void Shoot()
     {
-        Instantiate(Ball, transform.position, Quaternion.identity);
-
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
         {
-            this.(Vector2.down * Force, ForceMode2D.Impulse);
+            GameObject ballInstance = Instantiate(Ball, transform.position, Quaternion.identity);
+            Rigidbody2D rb = ballInstance.GetComponent<Rigidbody2D>();
+            
+            rb.AddForce(Vector2.down * Force, ForceMode2D.Impulse);
+           
         }
     }
 }
